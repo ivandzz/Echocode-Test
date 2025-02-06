14d79a88aaf77baa5258fca76c5f24e592b34c13
Ivan Dzhulynskyi <ivan.dzhul@gmail.com>
2025-02-06 15:51:48 +0200

    Initial Commit

---
+++ Echocode-Test/Echocode_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Echocode_TestApp.swift
+//  Echocode-Test
+//
+//  Created by Іван Джулинський on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Echocode_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
