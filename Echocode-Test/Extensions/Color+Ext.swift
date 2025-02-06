02bd56e9f15090397087f49e16ba4e8a35b46f9e
Ivan Dzhulynskyi <ivan.dzhul@gmail.com>
2025-02-06 18:09:08 +0200

    feat: Add custom color extension

---
+++ Echocode-Test/Extensions/Color+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Ext.swift
+//  Echocode-Test
+//
+//  Created by Іван Джулинський on 06.02.2025.
+//
+
+import SwiftUI
+
+extension Color {
+    static let customBlue = Color(red: 0.8, green: 0.9, blue: 1.0)
+    static let customYellow = Color(red: 0.9, green: 1.0, blue: 0.8)
+}
